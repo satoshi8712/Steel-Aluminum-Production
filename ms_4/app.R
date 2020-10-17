ede9512799cff1b057948dbe02fc69d2f3bee982
satoshi8712 <you@example.com>
2020-10-18 03:10:54 +0900

    commit

--- ms_4/app.R
+++ ms_4/app.R
@@ -9,6 +9,28 @@
 library(shiny)
 library(PPBDS.data)
 library(tidyverse)
+
+DP_LIVE_17102020190158566 <- read_csv("data/DP_LIVE_17102020190158566.csv", 
+                                      col_names = TRUE, 
+                                      cols(
+    LOCATION = col_character(),
+    INDICATOR = col_character(),
+    SUBJECT = col_character(),
+    MEASURE = col_character(),
+    FREQUENCY = col_character(),
+    TIME = col_double(),
+    Value = col_double(),
+    `Flag Codes` = col_character()
+))
+
+API_BM.KLT.DINV.CD.WD_DS2_en_csv_v2_1497113 <- read_csv("data/API_BM.KLT.DINV.CD.WD_DS2_en_csv_v2_1497113.csv", 
+                                                        col_names = TRUE, 
+                                                        cols(
+    `Data Source` = col_character(),
+    `World Development Indicators` = col_character(),
+    X3 = col_character()
+))
+    
 # Define UI for application that draws a histogram
 ui <- navbarPage(
     "Final Project Title",
@@ -26,13 +48,15 @@ ui <- navbarPage(
              h3("Project Background and Motivations"),
              p("Hello, this is where I talk about my project."),
              h3("About Me"),
-             p("My name is ______ and I study ______. 
-             You can reach me at ______@college.harvard.edu.")))
+             p("My name is Satoshi Yanaizu and I study Social Studies. 
+             You can reach me at satoshi_yanaizu@college.harvard.edu.")))
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
     output$line_plot <- renderPlot({
-       ggplot(PPBDS.data::sps, aes(age, education)) +
+       DP_LIVE_17102020190158566 %>%
+            filter(TIME == 2012) %>%
+            ggplot(aes(LOCATION, Value)) +
             geom_point()
     })
 }
